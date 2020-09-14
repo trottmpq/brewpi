2dcc7367e36917d0bf2e1b96763bf7a43acdb312
Matt Trott <trottmpq@gmail.com>
2020-09-14 17:12:50 +0100

    Can now Post to Heater api from front end

--- reactbrew/src/views/HeaterView/heaterpost.js
+++ reactbrew/src/views/HeaterView/heaterpost.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import Fade from '@material-ui/core/Fade';
-import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
+import Backdrop from '@material-ui/core/Backdrop';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Divider from '@material-ui/core/Divider';
+import Fab from '@material-ui/core/Fab';
+import Modal from '@material-ui/core/Modal';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 const useStyles = makeStyles(theme => ({
   modal: {
@@ -68,48 +70,100 @@ export default function HeaterForm() {
           timeout: 500
         }}
       >
-        <Fade in={open}>
-          <Paper>
-            <form>
-            <Typography variant="h1" component="h2">
-            Add Heater
-          </Typography>
-              <List>
-                <ListItem alignItems="center">
-                  <Typography color="textSecondary" align-items="flex-end">
-                    Name:
-                    <TextField
-                      id="outlined-basic"
-                      label="Name"
-                      variant="outlined"
-                    />
-                  </Typography>
-                </ListItem>
-                <ListItem>
-                  <Typography color="textSecondary">
-                    GPIO Number:
-                    <TextField
-                      id="outlined-basic"
-                      label="GPIO Number"
-                      variant="outlined"
-                    />
-                  </Typography>
-                </ListItem>
-                <ListItem>
-                  {' '}
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    type="submit"
-                    value="Submit"
-                  >
-                    Submit
-                  </Button>
-                </ListItem>
-              </List>
-            </form>
-          </Paper>
-        </Fade>
+        <Paper>
+          <Container maxWidth="sm">
+            <Formik
+              initialValues={{ name: '', gpio_num: '' }}
+              validationSchema={Yup.object().shape({
+                name: Yup.string()
+                  .max(255)
+                  .required('Name is required'),
+                gpio_num: Yup.number()
+                  .integer()
+                  .required('GPIO Number is required')
+              })}
+              onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                  fetch('/api/heater', {
+                    method: 'POST',
+                    body: JSON.stringify(values, null, 2),
+                    headers: { 'Content-Type': 'application/json' }
+                  })
+                    .then(res => res.json())
+                    .catch(error => console.error('Error:', error))
+                    .then(response => console.log('Success:', response));
+                  setSubmitting(false);
+                  handleClose();
+                }, 400);
+              }}
+            >
+              {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting
+                /* and other goodies */
+              }) => (
+                <form onSubmit={handleSubmit}>
+                  <Box mb={3}>
+                    <Typography color="textPrimary" variant="h2">
+                      Create new Heater
+                    </Typography>
+                    <Typography
+                      color="textSecondary"
+                      gutterBottom
+                      variant="body2"
+                    >
+                      Use this Form to create a new heater.
+                    </Typography>
+                  </Box>
+                  <Divider />
+                  <TextField
+                    type="text"
+                    name="name"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.name}
+                    error={Boolean(touched.name && errors.name)}
+                    fullWidth
+                    helperText={touched.name && errors.name}
+                    label="Name"
+                    margin="normal"
+                    variant="outlined"
+                  />
+                  <TextField
+                    type="number"
+                    name="gpio_num"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.gpio_num}
+                    error={Boolean(touched.gpio_num && errors.gpio_num)}
+                    fullWidth
+                    helperText={touched.gpio_num && errors.gpio_num}
+                    label="GPIO Number"
+                    margin="normal"
+                    variant="outlined"
+                  />
+                  <Box my={2}>
+                    <Button
+                      color="primary"
+                      disabled={isSubmitting}
+                      fullWidth
+                      size="large"
+                      type="submit"
+                      variant="contained"
+                    >
+                      Submit
+                    </Button>
+                  </Box>
+                </form>
+              )}
+            </Formik>
+          </Container>
+        </Paper>
       </Modal>
     </div>
   );

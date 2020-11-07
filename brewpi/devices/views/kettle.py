c9bb4bc0a392ac0ddfb6cb02bfda568ecc7413da
Matt Trott <trottmpq@gmail.com>
2020-11-07 18:55:32 +0000

    added useful kettle endpoints

--- brewpi/devices/views/kettle.py
+++ brewpi/devices/views/kettle.py
@@ -4,6 +4,9 @@ from flask_restx import Namespace, Resource, fields
 
 from ..models import Kettle
 from ..schemas import KettleSchema
+from .heater import nsmodel as heatermodel
+from .pump import nsmodel as pumpmodel
+from .temp_sensor import nsmodel as tempsensormodel
 
 api = Namespace("Kettle", description="Kettle related operations")
 
@@ -14,6 +17,36 @@ nsmodel = api.model(
         "name": fields.String(required=True, description="Kettle Name"),
         "target_temp": fields.Float(
             default=0.0, description="Kettle target temperature"
+        ),
+        "temp_sensor": fields.Nested(tempsensormodel, allow_null=True),
+        "pump": fields.Nested(pumpmodel, allow_null=True),
+        "heater": fields.Nested(heatermodel, allow_null=True),
+    },
+)
+
+nsmodeltemp = api.model(
+    "KettleTemperature",
+    {
+        "temperature": fields.Float(
+            default=0.0, description="Temperature in degrees Celcius"
+        ),
+    },
+)
+
+nsmodelheaterstate = api.model(
+    "KettleHeaterState",
+    {
+        "state": fields.Boolean(
+            default=False, description="True = Heater On, False = Heater Off"
+        ),
+    },
+)
+
+nsmodelpumpstate = api.model(
+    "KettlePumpState",
+    {
+        "state": fields.Boolean(
+            default=False, description="True = Pump On, False = Pump Off"
         ),
     },
 )
@@ -29,6 +62,9 @@ class KettleList(Resource):
         """List all Kettles."""
         schema = KettleSchema(many=True)
         query = Kettle.query.all()
+        for kettle in query:
+            if kettle.temp_sensor:
+                kettle.temp_sensor.current_temperature
         return schema.dump(query)
 
     @api.doc("create_kettle")
@@ -44,3 +80,133 @@ class KettleList(Resource):
             new_item = Kettle.create(**data)
             return schema.jsonify(new_item)
         return api.abort(404, message="Invalid Fields. Cannot Create Item")
+
+
+@api.route("/<id>")
+@api.param("id", "The Kettle identifier")
+@api.response(404, "Kettle not found")
+class KettleItem(Resource):
+    """Retrieve a kettle instance."""
+
+    @api.doc("get_kettle")
+    @api.marshal_with(nsmodel)
+    def get(self, id):
+        """Fetch a Kettle given its identifier."""
+
+        schema = KettleSchema()
+        query = Kettle.get_by_id(id)
+        if not query:
+            api.abort(404, message="Kettle {} doesn't exist".format(id))
+        if query.temp_sensor:
+            query.temp_sensor.current_temperature
+        return schema.dump(query)
+
+    @api.doc("delete_kettle")
+    @api.response(204, "Kettle deleted")
+    @api.response(404, "Kettle does not exist")
+    def delete(self, id):
+        """Delete a kettle given its identifier."""
+        kettle = Kettle.get_by_id(id)
+        if not kettle:
+            api.abort(404, message=f"Kettle {id} doesn't exist")
+        kettle.delete()
+        return "", 204
+
+    @api.expect(nsmodel)
+    @api.marshal_with(nsmodel)
+    def put(self, id):
+        """Update a kettle given its identifier."""
+        schema = KettleSchema()
+        kettle = Kettle.get_by_id(id)
+        if not kettle:
+            api.abort(404, message=f"Kettle {id} doesn't exist")
+
+        data = schema.load(request.get_json(), partial=True)
+        if data:
+            for key, value in data.items():
+                if value is not None:
+                    if hasattr(kettle, key):
+                        setattr(kettle, key, value)
+            kettle.update()
+            return schema.dump(kettle)
+        return api.abort(404, message="Invalid Fields. Cannot Update kettle")
+
+
+@api.route("/<id>/temperature")
+@api.param("id", "The kettle identifier")
+@api.response(404, "kettle tempsensor not found")
+class KettleItemTemperature(Resource):
+    """Retrieve a temp_sensor instance."""
+
+    @api.doc("get_kettle_temperature")
+    @api.doc(model=nsmodeltemp)
+    @api.marshal_with(nsmodeltemp)
+    def get(self, id):
+        """Fetch the current temp_sensor state."""
+
+        # schema = KettleSchema()
+        query = Kettle.get_by_id(id)
+        if not query:
+            api.abort(404, message=f"kettle {id} doesn't exist")
+        if not query.temp_sensor:
+            api.abort(
+                404, message=f"kettle {id} doesn't have a temp sensor configured."
+            )
+
+        temperature = query.temp_sensor.current_temperature
+        current_app.logger.info(
+            f"{query.name}, {query.temp_sensor.name} on gpio {query.temp_sensor.gpio_num} reads {temperature:.2f}C"
+        )
+        return {"temperature": temperature}
+
+
+@api.route("/<id>/heaterstate")
+@api.param("id", "The kettle identifier")
+@api.response(404, "kettle heater not found")
+class KettleItemHeaterState(Resource):
+    """Retrieve a heater instance from the kettle."""
+
+    @api.doc("get_kettle_heater")
+    @api.doc(model=nsmodelheaterstate)
+    @api.marshal_with(nsmodelheaterstate)
+    def get(self, id):
+        """Fetch the current heater state."""
+
+        # schema = KettleSchema()
+        query = Kettle.get_by_id(id)
+        if not query:
+            api.abort(404, message=f"kettle {id} doesn't exist")
+        if not query.heater:
+            api.abort(404, message=f"kettle {id} doesn't have a heater configured.")
+
+        current_state = query.heater.current_state
+        current_app.logger.info(
+            f"{query.name}, {query.heater.name} on gpio {query.heater.gpio_num} reads {current_state}"
+        )
+        return {"state": current_state}
+
+
+@api.route("/<id>/pumpstate")
+@api.param("id", "The kettle identifier")
+@api.response(404, "kettle pump not found")
+class KettleItemPumpState(Resource):
+    """Retrieve a pump instance from the kettle."""
+
+    @api.doc("get_kettle_pump")
+    @api.doc(model=nsmodelpumpstate)
+    @api.marshal_with(nsmodelpumpstate)
+    def get(self, id):
+        """Fetch the current pump state."""
+
+        # schema = KettleSchema()
+        query = Kettle.get_by_id(id)
+        if not query:
+            api.abort(404, message=f"kettle {id} doesn't exist")
+        if not query.pump:
+            api.abort(404, message=f"kettle {id} doesn't have a pump configured.")
+
+        current_state = query.pump.current_state
+        current_app.logger.info(
+            f"{query.name}, {query.pump.name} on gpio {query.pump.gpio_num} reads {current_state}"
+        )
+        return {"state": current_state}

e92dc142ecd5a42aa3a55b1829b03fa38023f316
Henry Lovett <seblovett@googlemail.com>
2020-11-15 09:34:49 +0000

    Fixed code

--- brewpi/misc/hysteresis.py
+++ brewpi/misc/hysteresis.py
@@ -5,10 +5,12 @@ import time
 
 def get_temp(kettle_id):
     resp = requests.get(f'http://localhost:5000/devices/Kettle/{kettle_id}/temperature')
+    print(resp.json())
     return resp.json().get("temperature")
 
 def get_targettemp(kettle_id):
     resp = requests.get(f'http://localhost:5000/devices/Kettle/{kettle_id}/targettemp')
+    print(resp.json())
     return resp.json().get("temperature")
 
 def set_heater(kettle_id, onoff):
@@ -16,23 +18,30 @@ def set_heater(kettle_id, onoff):
         print("Heater ON")
     else:
         print("Heater OFF")
-    #requests.put(f'http://127.0.0.1:5000/devices/Kettle/{kettle_id}/heaterstate', json={'state': onoff})
+    requests.put(f'http://127.0.0.1:5000/devices/Kettle/{kettle_id}/heaterstate', json={'state': onoff})
 
 def get_hyst_window(kettle_id):
     resp = requests.get(f'http://localhost:5000/devices/Kettle/{kettle_id}')
+    print(resp.json())
     return resp.json().get("hyst_window")
 
 def temp_loop(kettle_id):
     """Hysterises loop to turn hold the kettle as a set temperature."""
+    heaterOn = False
     while True:
         temp_c = get_temp(kettle_id)  # Current temperature
         target_temp = get_targettemp(kettle_id)
         hyst_window = get_hyst_window(kettle_id)
+        
         print(f"{temp_c},{target_temp},{hyst_window},")
-        if temp_c > (target_temp + hyst_window):
-            set_heater(kettle_id, False)
-        if temp_c < (target_temp - hyst_window):
-            set_heater(kettle_id, True)
+        if heaterOn:
+            if temp_c > (target_temp + hyst_window):
+                heaterOn = False
+                set_heater(kettle_id, heaterOn)
+        else:
+            if temp_c < (target_temp - hyst_window):
+                heaterOn = True
+                set_heater(kettle_id, heaterOn)
         time.sleep(5)
 
 if "__main__" == __name__:

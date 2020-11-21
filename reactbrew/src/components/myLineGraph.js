86e57cd332104add95efccf6def74bdfa38ad1ef
Henry Lovett <seblovett@googlemail.com>
2020-11-21 16:04:17 +0000

    Graphy stuff

--- reactbrew/src/components/myLineGraph.js
+++ reactbrew/src/components/myLineGraph.js
@@ -10,7 +10,8 @@ let myLineChart;
 
 export default class LineGraph extends Component {
     chartRef = React.createRef();
-    // myChartRef = undefined;
+    
+    state = { time : [], data1 : [], data2 : [], data3 : []};
     componentDidMount() {
         this.buildChart();
         this.dataListId = setInterval(() => this.getapi(), 1000);
@@ -28,16 +29,33 @@ export default class LineGraph extends Component {
         fetch("/api/devices/Kettle/1/temperature")
           .then(res => res.json())
           .then(data => {
-            this.props.data.push(data.temperature)
-            this.props.labels.push(String(new Date().getSeconds()))
-            // console.log(this.props.data)
+            this.state.data1.push(data.temperature)
+            this.state.time.push(new Date())
             this.buildChart()
           })
           .catch(console.log);
+
+          fetch("/api/devices/Kettle/2/temperature")
+          .then(res => res.json())
+          .then(data => {
+            this.state.data2.push(data.temperature)
+            this.buildChart()
+          })
+          .catch(console.log);
+
+          fetch("/api/devices/Kettle/3/temperature")
+          .then(res => res.json())
+          .then(data => {
+            this.state.data3.push(data.temperature)
+            this.buildChart()
+          })
+          .catch(console.log);
+
+
       }
     buildChart = () => {
         const myChartRef = this.chartRef.current.getContext("2d");
-        const { data, labels } = this.props;
+        // const { data, labels } = this.props;
 
         if (typeof myLineChart !== "undefined") myLineChart.destroy();
 
@@ -45,19 +63,47 @@ export default class LineGraph extends Component {
             type: "line",
             data: {
                 //Bring in data
-                labels: labels,
+                labels: this.state.time,
                 datasets: [
                     {
                         label: "Kettle 1 Temperature",
-                        data: data,
+                        data: this.state.data1,
                         fill: false,
                         borderColor: "#6610f2"
+                    },
+                    {
+                        label: "Kettle 2 Temperature",
+                        data: this.state.data2,
+                        fill: false,
+                        borderColor: "#0010f2"
+                    },
+                    {
+                        label: "Kettle 3 Temperature",
+                        data: this.state.data3,
+                        fill: false,
+                        borderColor: "#661000"
                     }
                     
                 ]
             },
             options: {
                 //Customize chart options
+                scales: {
+                    yAxes: [{
+                        type: 'linear',
+                        ticks :{
+                            min : 0,
+                            max : 100
+                        },
+                    }],
+                    xAxes: [{
+                        type: 'time',
+                        time: {
+                            unit: 'second',
+                            stepSize : 10
+                        }
+                    }]
+                },
                 animation: {
                     duration: 0
                 }

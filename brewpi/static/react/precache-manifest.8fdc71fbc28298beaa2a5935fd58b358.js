e429f4628494b6b42119a952d9f4908c60d7ae5a
trottmpq <trottmpq@gmail.com>
2020-12-29 16:48:27 +0000

    updated yarn and rebuilt. tidy up package.json

---
+++ brewpi/static/react/precache-manifest.8fdc71fbc28298beaa2a5935fd58b358.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b2ae95537b08c62d838339e077191a5",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "315c5b718487be2fb7d5",
+    "url": "/static/react/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "b110e2d68587464e2138",
+    "url": "/static/react/css/main.e9ae2109.chunk.css"
+  },
+  {
+    "revision": "315c5b718487be2fb7d5",
+    "url": "/static/react/js/2.b27d46bb.chunk.js"
+  },
+  {
+    "revision": "168606c6d3ef023cc386d380e473c991",
+    "url": "/static/react/js/2.b27d46bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b110e2d68587464e2138",
+    "url": "/static/react/js/main.756b7d21.chunk.js"
+  },
+  {
+    "revision": "77ff06050a058800f67f",
+    "url": "/static/react/js/runtime-main.f425a421.js"
+  }
+]);
--- brewpi/static/react/service-worker.js
+++ brewpi/static/react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/static/react/precache-manifest.aa1f0ffbb48efa879239bb2b38621c52.js"
+  "/static/react/precache-manifest.8fdc71fbc28298beaa2a5935fd58b358.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/static/react/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
